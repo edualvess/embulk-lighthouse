fb505d3caff1b89ff1a2059a786a0763ac7a1958
Lucas <lucascmgs@gmail.com>
2023-01-24 19:34:09 -0300

    change entroypoint of embulk image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 #CONFIG_PATH="/usr/tmp/config.yml"
 
-if [ -z "${@}" ]; then
-   echo "Config file required!"
-   exit 1
+if [ -f /opt/embulk/plugins.txt ]; then
+    for plugin in `cat /opt/embulk/plugins.txt`; do
+        java -jar /opt/embulk/embulk.jar gem install $plugin
+    done
 fi
 
-/opt/embulk/embulk "${@}"
+CONFIG_DIR='/tmp/config.yml'
+echo "${@}"
+if [ -z "$BASE64_CONFIG" ]
+then
+    # NOT BASE64
+    echo "Running embulk without base64 config, a volume should be mapped"
+    exec java $JAVA_OPTS -jar /opt/embulk/embulk.jar "${@}"
+else
+    # BASE64
+    echo "Running embulk with base64 config, decoding string to $CONFIG_DIR"
+    echo $BASE64_CONFIG | base64 -d > $CONFIG_DIR
+    if [ -z "$INCREMENTAL" ]
+    then
+        # NOT INCREMENTAL
+        exec java $JAVA_OPTS -jar /opt/embulk/embulk.jar run $CONFIG_DIR
+    fi
+fi
